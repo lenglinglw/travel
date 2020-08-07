5ff5e8e7a9c0d976e32d0fe8d6cccc102597bd7b
suqiang <504274232@qq.com>
2020-08-07 19:40:00 +0800

    swiper库

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -6,10 +6,12 @@
 
 <script>
 import HomeHeader from './components/Header'
+import HomeSwiper from './components/Swiper'
 export default {
   name: 'Home',
   components: {
-    HomeHeader
+    HomeHeader,
+    HomeSwiper
   }
 }
 </script>
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <swiper :options="swiperOptions">
+        <swiper-slide>Slide 1</swiper-slide>
+        <swiper-slide>Slide 2</swiper-slide>
+        <swiper-slide>Slide 3</swiper-slide>
+        <swiper-slide>Slide 4</swiper-slide>
+        <swiper-slide>Slide 5</swiper-slide>
+        <div class="swiper-pagination" slot="pagination"></div>
+  </swiper>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOptions: {
+        pagination: {
+          el: '.swiper-pagination'
+        }
+        // Some Swiper option/callback...
+      }
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
